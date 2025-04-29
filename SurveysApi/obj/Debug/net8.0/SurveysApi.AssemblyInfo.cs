9dcc9b89c33beb3a1813846db2a82f0de6f9dc22
Edson Gómez <lsc.edsongomez@gmail.com>
2025-04-29 21:28:39 +0000

    update

--- SurveysApi/obj/Debug/net8.0/SurveysApi.AssemblyInfo.cs
+++ SurveysApi/obj/Debug/net8.0/SurveysApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurveysApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9906905c849b9b055b58367274dad4e7bca8f0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305d935f39317447293b9f27a8c55bbf66b23e2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurveysApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SurveysApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
